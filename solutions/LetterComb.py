88ccbb5020e1bcfe3ea606e624737757ca4754d5
Volkov Grigoriy <grihanaft0510@gmail.com>
2021-09-09 10:40:23 +0300

    Added Letter Combinations of a Phone Number
    https://leetcode.com/problems/letter-combinations-of-a-phone-number/submissions/

---
+++ solutions/LetterComb.py
@@ -0,0 +1,16 @@
+def letterCombinations(digits: str) -> List[str]:
+    if len(digits) == 0:
+        return []
+    num2letters = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    }
+
+    return list(map(lambda x: ''.join(x),
+    product(*(num2letters[ch] for ch in digits))))
