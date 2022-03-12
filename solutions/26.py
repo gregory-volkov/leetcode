3a905ae5cefc3569e3d674704b6ce181051a9a61
Volkov Grigoriy <grihanaft0510@gmail.com>
2022-03-12 13:28:43 +0300

    Update and rename removeDupSorted.py to 26.py

--- solutions/26.py
+++ solutions/26.py
@@ -1,3 +1,6 @@
+# 26. Remove Duplicates from Sorted Array
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
 def removeDuplicates(nums: List[int]) -> int:
     j = 1
     for i in range(1, len(nums)):
