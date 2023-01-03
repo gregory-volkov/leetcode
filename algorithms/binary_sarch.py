93f1f1505ac471cdaf429714bfa792134f759b24
Volkov Grigoriy <grihanaft0510@gmail.com>
2023-01-03 18:24:18 +0400

    Update binary_sarch.py

--- algorithms/binary_sarch.py
+++ algorithms/binary_sarch.py
@@ -1,3 +1,17 @@
+# Classic one
+# Just returns the id if persists, otherwise -1
+def bsearch(nums, target):
+    i, j = 0, len(nums) - 1
+    while i <= j:
+        mid = (i + j) // 2
+        if target < nums[mid]:
+            j = mid - 1
+        if target > nums[mid]:
+            i = mid + 1
+        if target == nums[mid]:
+            return mid
+    return -1
+
 # Upper bound
 # Returns the id of required item or the place where it should be
 def bsearch_upper(lst, item):
