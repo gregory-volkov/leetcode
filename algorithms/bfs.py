849436c3f4e31daaf5a7dab0b44a1f192b23df9f
Volkov Grigory <grihanaft0510@gmail.com>
2023-02-22 22:19:02 +0400

    Create bfs.py

---
+++ algorithms/bfs.py
@@ -0,0 +1,30 @@
+# Just bfs traversal of a graph
+# O(V+E) where V is the number of vertices and E is the number of edges
+
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+visited = [] # List to keep track of visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+bfs(visited, graph, 'A')
